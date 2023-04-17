c5bf55226858ce3785d3aa1fb782b594d5a20c59
ashleylem <ashley.lemus@yahoo.com>
2023-04-17 16:56:09 +0000

    done

--- src/app.js
+++ src/app.js
@@ -10,4 +10,17 @@ window.onload = function() {
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
+  let domains = [".com", ".net", ".us", ".io"];
+  let domainNames = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < noun.length; j++) {
+      for (let k = 0; k < adj.length; k++) {
+        for (let l = 0; l < domains.length; l++) {
+          domainNames.push(pronoun[i] + adj[k] + noun[j] + domains[l]);
+        }
+      }
+    }
+  }
+  console.log(domainNames);
 };
